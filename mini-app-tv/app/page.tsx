d76cc7f200c947570190de5781a507e57c65a853
sirgawain0x <bracy.gawain2014@gmail.com>
2025-05-16 22:40:52 -0400

    overlap bugfix
    - dropdown fix.

--- mini-app-tv/app/page.tsx
+++ mini-app-tv/app/page.tsx
@@ -20,7 +20,7 @@ export default function App() {
   return (
     <div className="flex flex-col min-h-screen font-sans text-[var(--app-foreground)] mini-app-theme from-[var(--app-background)] to-[var(--app-gray)]">
       <div className="w-full max-w-md mx-auto px-4 py-3">
-        <header className="flex justify-between items-center mb-3 h-11">
+        <header className="flex justify-between items-center mb-3 h-11 z-50 relative">
           <div>
             <div className="flex items-center space-x-2">
               <Wallet className="z-10">
